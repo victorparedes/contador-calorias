6327b3ef1a30cc2f391845bd9ed441e56384549c
vparedes <tatoman@gmail.com>
2023-12-24 20:44:01 -0300

    .

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaView, View, Text, StyleSheet, Platform, StatusBar } from "react-native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from './src/components/screens/home';
+import AddFood from './src/components/screens/addFood'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home' >
+        <Stack.Screen name='Home' component={Home} options={{headerShown: false}}/>
+        <Stack.Screen name='AddFood' component={AddFood} options={{headerShown: false}} />
+      </Stack.Navigator>
+      </NavigationContainer> 
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
 });
